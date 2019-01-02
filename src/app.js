5655c7977748d519c60fab8d4f9fdf5406f4b055
edwinwalela <edwinwalela@gmail.com>
2019-01-02 13:37:07 +0300

    quote slider"

--- src/app.js
+++ src/app.js
@@ -25,7 +25,7 @@ app.use((req,res,next)=>{
 	}else{
 		sort = -1;
 	}
-	Book.find({}).skip(17).limit(4).sort({price:sort}).then(titles=>{
+	Book.find({}).skip(27).limit(5).sort({price:sort}).then(titles=>{
 		res.locals = titles;
 		next();
 	})
