9033d1a2713048a1cc05f16c2b455f6b71e919db
edwinwalela <edwinwalela@gmail.com>
2019-02-02 21:29:41 +0300

    port change to 8080

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 module.exports = {
     "DB_URI":"mongodb://admin:admin3000@ds243344.mlab.com:43344/booked",
     "FB_APP_ID":"2213682165548965",
-    "PORT":80,
+    "PORT":8080,
     "FB_APP_SECRET":"ee97b08467cda894427f1df291f6a95e",
     "TINIFYKEY":"6VlZNBRRYKg7pTyDjGHLyt1Xq1jpT6ns"
 }
